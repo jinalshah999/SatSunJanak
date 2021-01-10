a9aacc2884c3a0e09bb6328389d81c7490b4aedf
jinalshah999 <jinalshah999@gmail.com>
2021-01-10 09:11:52 +0530

    crud with arr

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -6,6 +6,12 @@ import { Todo } from './todo';
   styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  flag1: boolean = true;
+  flag: boolean = false;
+  statusArr: string[] = ['done', 'pending'];
+  Id: string;
+  Title: string;
+  Status: string;
   todosArr: Todo[] = [
     new Todo('1', 'go for running', 'pending'),
     new Todo('2', 'Email to your manager', 'pending'),
@@ -16,5 +22,39 @@ export class TodosComponent implements OnInit {
   ngOnInit(): void {}
   onDelete(item: Todo): void {
     this.todosArr.splice(this.todosArr.indexOf(item), 1);
+  }
+  onEdit(item: Todo): void {
+    if (item.Status == 'done') {
+      item.Status = 'pending';
+    } else {
+      item.Status = 'done';
+    }
+  }
+  onAddButtonClick(): void {
+    this.flag = true;
+  }
+  onCancelButtonClick(): void {
+    this.flag = false;
+  }
+  onSaveButtonClick(): void {
+    this.todosArr.push(new Todo(this.Id, this.Title, this.Status));
+    this.Id = '';
+    this.Title = '';
+    this.Status = '';
+    this.flag = false;
+  }
+  onRefreshClick() {
+    this.todosArr = [
+      new Todo('1', 'go for running', 'pending'),
+      new Todo('2', 'Email to your manager', 'pending'),
+      new Todo('3', 'push your code to github', 'done'),
+      new Todo('4', 'sample', 'pending'),
+    ];
+  }
+  trackByTodoId(todo: Todo) {
+    return todo.Id;
+  }
+  onClickMe() {
+    this.flag1 = !this.flag1;
   }
 }
