d9fa07d74610b29e22078063c626815eb259306e
jinalshah999 <jinalshah999@gmail.com>
2021-02-20 09:44:37 +0530

    reacitve forms

--- src/app/user-signup/user-signup.component.ts
+++ src/app/user-signup/user-signup.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {
   AbstractControl,
+  FormArray,
   FormControl,
+  FormControlDirective,
   FormGroup,
   Validators,
 } from '@angular/forms';
@@ -15,7 +17,7 @@ export class UserSignupComponent implements OnInit {
   user_signup: FormGroup;
 
   invalidNames: string[] = ['jinal', 'shah'];
-
+  cities: string[] = ['ahmedabad', 'baroda', 'surat'];
   invalidName(c: AbstractControl): { [key: string]: boolean } | null {
     if (this.invalidNames.indexOf(c.value) >= 0) {
       return { customError: true };
@@ -37,7 +39,12 @@ export class UserSignupComponent implements OnInit {
         Validators.pattern('[a-zA-Z]*'),
         this.invalidName.bind(this),
       ]),
-      user_email: new FormControl(null),
+      user_email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+      ]),
+      user_mobile: new FormControl(null),
+      user_notification: new FormControl('email'),
       password_group: new FormGroup(
         {
           user_password: new FormControl(null, Validators.required),
@@ -45,9 +52,39 @@ export class UserSignupComponent implements OnInit {
         },
         this.matchPassword.bind(this)
       ),
+      user_hobbies: new FormArray([]),
+      user_city: new FormControl('ahmedabad', Validators.required),
     });
+
+    this.user_signup
+      .get('user_notification')
+      .valueChanges.subscribe((x) => this.setValidations(x));
+  }
+  setValidations(value: string): void {
+    let emailcontrol = this.user_signup.get('user_email');
+    let phonecontrol = this.user_signup.get('user_mobile');
+    if (value == 'email') {
+      phonecontrol.clearValidators();
+      emailcontrol.setValidators([Validators.required, Validators.email]);
+    } else {
+      emailcontrol.clearValidators();
+      phonecontrol.setValidators([Validators.required]);
+      emailcontrol.setValidators([Validators.email]);
+    }
+    emailcontrol.updateValueAndValidity();
+    phonecontrol.updateValueAndValidity();
+  }
+  getHobbies() {
+    return (<FormArray>this.user_signup.get('user_hobbies')).controls;
   }
   onUserAdd() {
     console.log('frm values', this.user_signup);
+  }
+  onDeleteHobbyClick(i) {
+    (<FormArray>this.user_signup.get('user_hobbies')).removeAt(i);
+  }
+  onAddHobbyClick() {
+    let cntrl = new FormControl(null);
+    (<FormArray>this.user_signup.get('user_hobbies')).push(cntrl);
   }
 }
